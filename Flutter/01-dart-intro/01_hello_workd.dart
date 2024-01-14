0d644972b9ab830e9e1a8cd83a2d1dbdda74ccfa
sanchezJose07 <106000125+sanchezJose07@users.noreply.github.com>
2024-01-14 13:59:46 -0600

    Add files via upload

---
+++ Flutter/01-dart-intro/01_hello_workd.dart
@@ -0,0 +1,11 @@
+void main(){
+  //var myName = 'JOSE';
+  //final myName = 'JOSE'; //NO PUEDE CAMBIAR DE VALOR
+  String myName = 'JOSE';
+  
+  myName = 'Mundo';
+  
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase() }');
+  print('Hola ${ 1 + 1}' ); 
+}
---
+++ Flutter/01-dart-intro/03_maps.dart
@@ -0,0 +1,21 @@
+void main(){
+ 
+  final Map<String, dynamic> pokemon = {
+    'name':'Ditto',
+    'hp': 100,
+    'isAlive' : true,
+    'abilities' : <String>['impostor'],
+    'sprites' : {
+      1 : 'ditto/front.png',
+      2 : 'ditto/back.png'
+      }
+  };
+  
+  print( pokemon );
+  print( 'Name: ${ pokemon['name'] }' );
+  print( 'Name: ${ pokemon['sprites'] } ');
+  
+  print( 'back: ${ pokemon['sprites'][2] } ' );
+  print( 'front: ${ pokemon['sprites'][1] } ' );
+  
+}
